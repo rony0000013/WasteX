8ad056ce75c74c886c7c74fac769bce07e72dff9
Soumyajit Mondal <mondalsoumya02@gmail.com>
2023-08-12 15:49:51 +0000

    created blog card

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,53 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Card from './Card';
 import waste1 from '../assets/waste1.jpg';
 import waste2 from '../assets/waste2.jpg';
 import waste3 from '../assets/waste3.jpg';
 import waste4 from '../assets/waste4.jpg';
+const wastes = [waste1, waste2, waste3, waste4]
+import { For } from "million"
+import axios from "axios";
 export default function Blog() {
+    const [isShowMore, setIsShowMore] = useState(false);
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(
+        axios.get(import.meta.env.VITE_BACKEND_API+"/blogs/").then((res) => {
+            setBlogs(res.data.blogs);
+        }).catch((err) => {
+            console.log(err)
+        })
+        , []);
+    const toggleReadMoreLess = () => {
+        setIsShowMore(!isShowMore);
+    };
     return (
         <>
             <div className="bg-primary w-full min-h-screen p-4 flex flex-wrap justify-center gap-4">
-            <div className="card w-full sm:w-96 bg-base-100 shadow-xl">
-                <figure><img src={waste1} alt="Waste" className="h-[150px]" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Read More</button>
-                    </div>
-                </div>
-            </div>
-            <div className="card w-full sm:w-96 bg-base-100 shadow-xl">
-                <figure><img src={waste2} alt="Shoes" className="h-[150px]"/></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Read More</button>
-                    </div>
-                </div>
-            </div>
-            <div className="card w-full sm:w-96 bg-base-100 shadow-xl">
-                <figure><img src={waste3} alt="Shoes" className="h-[150px]"/></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Read More</button>
-                    </div>
-                </div>
-            </div>
-            <div className="card w-full sm:w-96 bg-base-100 shadow-xl">
-                <figure><img src={waste4} alt="Shoes" className="h-[150px]"/></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Read More</button>
-                    </div>
-                </div>
-            </div>
-                
+               <For each={blogs}>
+                {(item, idx) =>
+                    <Card img={wastes[idx]}
+                        title={title}
+                        text={text}></Card>
+               }</For>
             </div>
         </>
     )
