38397fe336aa4dc8938cd85eeabe49f156cb877b
Soumyajit Mondal <mondalsoumya02@gmail.com>
2023-08-12 07:34:50 +0000

    Created the components and added the files in the folder

---
+++ src/components/Showcase.jsx
@@ -0,0 +1,20 @@
+import { block, For } from "million/react"
+// import blogs from "../store/blogs"
+import { useStore } from "@nanostores/react"
+
+// blogs.subscribe((blogs) => {
+//     console.log(blogs)
+// })
+
+
+const Showcase = block(() => {
+    // const blogs = useStore(blogs);
+    console.log(blogs)
+
+    return (<div className="flex ">
+        {/* <For each={}/> */}
+    <h1>Hello</h1>
+    </div>)
+})
+
+export default Showcase
---
+++ src/components/index.js
@@ -0,0 +1,22 @@
+import Billing from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Showcase from "./Showcase";
+
+export {
+    Navbar,
+    Hero,
+    Footer,
+    CTA,
+    CardDeal,
+    Billing,
+    Business,
+    Showcase,
+};
+
+
+
